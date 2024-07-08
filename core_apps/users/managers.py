69923ea5c14b88b216b7ca9ea6100c0f38b325ba
aabhishek777 <aabhishekksinghh@gmail.com>
2024-07-08 13:53:40 +0530

    add: custom user model

--- core_apps/users/managers.py
+++ core_apps/users/managers.py
@@ -58,7 +58,7 @@ class CustomUserManager(BaseUserManager):
         else:
             raise ValidationError(_("Email can not be empty"))
         
-        user= self.create_user(first_name, last_name, email, password)
+        user= self.create_user(first_name, last_name, email, password, **extra_fileds)
         
         user.save(using=self._db)
         
